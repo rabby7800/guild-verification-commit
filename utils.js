755a7ba8eed5ce2802d6ab75b72f5d5ba7eb76b4
rabby7800 <rabby780islam@gmail.com>
2026-03-11 10:44:04 +0600

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T04:43:30.929Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-11T04:44:16.721Z
+Commit: Fix CSS styling bug on mobile devices
